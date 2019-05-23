07112be41d4d16aec7ea7e26ff293cd33b2d7988
Raúl Contreras <raulc98@gmail.com>
2019-05-23 11:17:01 +0200

    The phones endpoint now returns the available phones

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const phones = require('../data/phones.json');
 
 /* GET home page. */
 router.get('/phones', function(req, res, next) {
   res.status(200).json({
-    message: 'Hello World'
+    data: phones
   });
 });
 
