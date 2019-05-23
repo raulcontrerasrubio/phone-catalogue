425695e7b513b90f6548ceb1072f9e9b037601ac
Raúl Contreras <raulc98@gmail.com>
2019-05-23 11:41:26 +0200

    Cors installed

--- server/app.js
+++ server/app.js
@@ -13,6 +13,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
+
 app.use('/', indexRouter);
 
 module.exports = app;
